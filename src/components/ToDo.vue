68acccaeff82e0e3f63c563b716ea4a60c51adb0
Angel H <angel@adboomgrp.com>
2021-08-09 16:08:10 -0600

    fixed responsive issues on updated sort button style

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -52,7 +52,7 @@
           v-if="toDoList.length"
           v-model="sort"
           placeholder="Sort"
-          fixedClasses="border-0 block w-44 px-3 py-2 transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+          fixedClasses="border-0 block w-52 sm:w-44 px-3 py-2 transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
           :class="[
             'text-xs sm:text-sm',
             {'text-gray-400': sort === 'Sort' || sort === ''}
